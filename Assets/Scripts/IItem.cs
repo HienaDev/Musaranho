3e70aa9fb92fa078139ecb035c937f2e767d05c3
HienaDev <ant.rodrigues1999@gmail.com>
2025-05-13 18:18:58 +0100

    Add fishing

---
+++ Assets/Scripts/IItem.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public interface IItem 
+{
+    void LeftClickItem();
+    void LeftHoldItem();
+
+    void LeftReleaseItem();
+
+    void RightClickItem();
+    void RightHoldItem();
+
+    void RightReleaseItem();
+}
