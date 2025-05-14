76ee59afccd91959d4a908be220232473a1e7798
André <imandreleandro@gmail.com>
2025-05-14 12:10:32 +0100

    abdd

--- Assets/AndreStuff/GameManager.cs
+++ Assets/AndreStuff/GameManager.cs
@@ -3,12 +3,14 @@ using System.Runtime.InteropServices;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
 
     [SerializeField] private PlayerControl playerControl;
+    [SerializeField] private Transform mainCanvas;
     
     private bool _canPlayerMove = false;
     public bool CanPlayerMove() => _canPlayerMove;
@@ -16,6 +18,8 @@ public class GameManager : MonoBehaviour
 
     private float _playerCameraSense = 2f;
     public float GetPlayerCameraSensitivity() => _playerCameraSense;
+    private float _crosshairSize = 10f;
+    public float GetCrosshairSize() => _crosshairSize;
 
     private float[] _dailyWeightNeeded;
     /// <summary>
@@ -75,6 +79,15 @@ public class GameManager : MonoBehaviour
         Cursor.visible = false;
         CenterCursor();
     }
+
+    private void UpdateCrosshairSize()
+    {
+        if (mainCanvas == null) return;
+        if (mainCanvas.Find("Crosshair").TryGetComponent(out RectTransform rectTransform))
+        {
+            rectTransform.sizeDelta = new Vector2(_crosshairSize, _crosshairSize);
+        }
+    }
     
     /*
      * MENU OPTIONS STUFF
@@ -83,8 +96,12 @@ public class GameManager : MonoBehaviour
     [Space][Header("Menu Options Stuff")]
     [SerializeField] private TMP_InputField sensitivityInputField;
     [SerializeField] private Slider sensitivitySlider;
-    [SerializeField] private float minSensitivity = 0.01f;
-    [SerializeField] private float maxSensitivity = 10f;
+    [SerializeField] private float sensitivityMin = 0.01f;
+    [SerializeField] private float sensitivityMax = 10f;
+    [SerializeField] private TMP_InputField crosshairSizeInputField;
+    [SerializeField] private Slider crosshairSizeSlider;
+    [SerializeField] private float crosshairSizeMin = 1f;
+    [SerializeField] private float crosshairSizeMax = 100f;
 
     public void ClickOptionsButton()
     {
@@ -97,24 +114,34 @@ public class GameManager : MonoBehaviour
         canvasMenu.transform.Find("Menu").gameObject.SetActive(true);
         canvasMenu.transform.Find("OptionsMenu").gameObject.SetActive(false);
     }
+
+    public void ChangedCrosshairSizeInput() => UpdateNewCrosshairSize(float.Parse(crosshairSizeInputField.text));
+
+    public void ChangedCrosshairSizeSlider() => UpdateNewCrosshairSize(crosshairSizeSlider.value);
+    private void UpdateCrosshairSizeInputFieldValue(string value) => crosshairSizeInputField.text = value;
+    private void UpdateCrosshairSizeSliderValue(float value) => crosshairSizeSlider.value = value;
+    private void UpdateNewCrosshairSize(float value)
+    {
+        value = Mathf.Max(value, crosshairSizeMin);
+        value = Mathf.Min(value, crosshairSizeMax);
+        value = Mathf.Round(value);
+        UpdateCrosshairSizeInputFieldValue($"{value:F0}");
+        UpdateCrosshairSizeSliderValue(value);
+        _crosshairSize = value;
+        UpdateCrosshairSize();
+    }
     
-    public void ChangedSenseInput()
-    {
-        UpdateNewSense(float.Parse(sensitivityInputField.text));
-    }
-    public void ChangedSenseSlider()
-    {
-        UpdateNewSense(sensitivitySlider.value);
-    }
-    private void UpdateInputFieldValue(string value) => sensitivityInputField.text = value;
-    private void UpdateSliderValue(float value) => sensitivitySlider.value = value;
+    public void ChangedSenseInput() => UpdateNewSense(float.Parse(sensitivityInputField.text));
+    public void ChangedSenseSlider() => UpdateNewSense(sensitivitySlider.value);
+    private void UpdateSensitivityInputFieldValue(string value) => sensitivityInputField.text = value;
+    private void UpdateSensitivitySliderValue(float value) => sensitivitySlider.value = value;
 
     private void UpdateNewSense(float value)
     {
-        value = Mathf.Max(value, minSensitivity);
-        value = Mathf.Min(value, maxSensitivity);
-        UpdateInputFieldValue($"{value:F2}");
-        UpdateSliderValue(value);
+        value = Mathf.Max(value, sensitivityMin);
+        value = Mathf.Min(value, sensitivityMax);
+        UpdateSensitivityInputFieldValue($"{value:F2}");
+        UpdateSensitivitySliderValue(value);
         _playerCameraSense = value;
         playerControl.UpdateCamSensitivity();
     }
