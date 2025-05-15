089a62abd6611f9926d7a3023a377bb75a86d438
André <imandreleandro@gmail.com>
2025-05-15 22:59:20 +0100

    abd

---
+++ Assets/AndreStuff/RatScript.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using UnityEngine;
+
+public class RatScript : MonoBehaviour
+{
+
+    [SerializeField] private float waitTimeForRun = 0.5f;
+    [SerializeField] private float moveSpeed = 1f;
+    
+    private Renderer _renderer;
+    private Camera _cam;
+    private bool _hasBeenSeen = false;
+    private Animator _animator;
+    private Rigidbody _rb;
+    private bool _shouldRun = false;
+    
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _cam = Camera.main;
+        _renderer = transform.Find("Rat").GetComponent<SkinnedMeshRenderer>();
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (_shouldRun) _rb.linearVelocity = transform.right * moveSpeed;
+        if (_hasBeenSeen) return;
+        if (IsSeen()) StartCoroutine(RatGotFoundAnime());
+    }
+    
+    private bool IsSeen()
+    {
+        Plane[] planes = GeometryUtility.CalculateFrustumPlanes(_cam);
+        return GeometryUtility.TestPlanesAABB(planes , transform.GetComponent<Collider>().bounds);
+    }
+
+    private IEnumerator RatGotFoundAnime()
+    {
+        Debug.Log("RAT SEEN!!");
+        _hasBeenSeen = true;
+        yield return new WaitForSeconds(waitTimeForRun);
+        _animator.SetBool("seen", true);
+        _shouldRun = true;
+        Color color = _renderer.material.color;
+        while (true)
+        {
+            color.a -= 1f * Time.deltaTime;
+            _renderer.material.color = color;
+            if (color.a < 0f) break;
+            yield return null;
+        }
+    }
+    
+    
+}
